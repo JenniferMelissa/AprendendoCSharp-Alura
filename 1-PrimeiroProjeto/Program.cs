0737cf248a770e39a4551a938bffe6e930fdebe8
Jennifer Melissa Martins de Sales <jennifer.sales@geap.com.br>
2022-11-21 11:44:50 -0300

    Primeiro Alô Mundo

---
+++ 1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PrimeiroProjeto
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo.Projeto no Visual Studio!");
+            Console.WriteLine("A execução acabou! Tecle enter para finalizar.");
+            //Console.ReadLine - Essa instrução vai parar a execução e ao clicar enter ela vai liberar a execução, se nao tive mais nada para ser executado, ela fecha a telinha
+            Console.ReadLine();
+        }
+    }
+}
+,
