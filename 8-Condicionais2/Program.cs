c8ddf0e9013c207e28bfc62f09b85a801251bc4a
Jennifer Melissa Martins de Sales <jennifer.sales@geap.com.br>
2022-11-22 09:40:39 -0300

    Operadores Logicos e Variavel Booleana

---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionais2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 8 - Condicionais 2!");
+
+            int idadeJoao = 18;
+            //int quantidadePessoas = 2;
+
+            //Variavel Booleana - True ou False
+            //bool acompanhado = quantidadePessoas >= 2;
+
+            bool acompanhado = true;
+
+            //Operador Lógico
+            // || --> OU ou OR
+            //&& --> E 
+            // == --> compara valores
+            // = --> atribuindo valores
+            if (idadeJoao >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar!");
+
+            }
+
+
+
+            Console.WriteLine("Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
