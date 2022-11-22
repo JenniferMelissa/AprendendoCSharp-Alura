c58a7698589cfb4b89a2034fff47be4cae888e02
Jennifer Melissa Martins de Sales <jennifer.sales@geap.com.br>
2022-11-22 13:00:07 -0300

    Desafio 5

---
+++ Desafio5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Utilize um laço do tipo for para imprimir todos os múltiplos de 3,
+            //entre 1 e 100.
+
+            for(int i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine();
+            //OU
+
+
+            for(int i = 3; i < 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
