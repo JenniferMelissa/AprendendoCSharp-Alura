f58a226069433fed87b7a19e08a474138f623297
Jennifer Melissa Martins de Sales <jennifer.sales@geap.com.br>
2022-11-22 10:16:44 -0300

    Escopo

---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Escopo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 9 - Escopo !");
+
+            int idadeJoao = 18;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "João está acompanhado!";
+            }
+            //pode tirar o "{}" do else quando tem apenas uma instrução dentro, apenas UMA LINHA,s e tiver oura linha não vai fazer parte do else mais
+            else
+            {
+                mensagemAdicional = ("Jpão não está acompanhado!");
+            }
+                 
+            
+        
+
+            
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar!");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar!");
+
+            }
+
+
+
+            Console.WriteLine("Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
